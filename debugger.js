1300cd97cd51f3a490d334036b98d5a5a2d9715a
rollbot <wqs1314xe@gmail.com>
2023-09-17 04:25:16 +0800

    chore: init

---
+++ debugger.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const webpackOptions = require('./webpack.config');
+debugger;
+const compiler = webpack(webpackOptions);
+
+compiler.run((err, stats) => {
+  console.log(err);
+  console.log(
+    stats.toJson({
+      entries: true,
+      chunks: true,
+      modules: false,
+      assets:false,
+    })
+  );
+})
